55d8de0114d79f883a7464f07362a05cd623c20a
Dimitri Charneux <dimitri15.59@hotmail.fr>
2016-02-20 18:48:14 +0100

    avancée dans le TP5, il ne reste qu'à rédiger un peu mieux

--- TP5/Fichiers_utiles_TP5_2016/segmente Bayes images 2 classes etudiant.R
+++ TP5/Fichiers_utiles_TP5_2016/segmente Bayes images 2 classes etudiant.R
@@ -23,7 +23,8 @@ source ("rdfSegmentation.R")
 
 # Chargement de l'image
 #nom <- "rdf-2-classes-texture-0.png"
-nom <- "2classes_100_100_8bits_2016.png"
+#nom <- "2classes_100_100_8bits_2016.png"
+nom <- "rdf-chiffre-0-8bits.png"
 image <- rdfReadGreyImage (nom)
 
 # Calcul et affichage de son histogramme
@@ -44,12 +45,13 @@ binaire60 <- (image - seuil) >= 0
 if (interactive ()) {
   #display (binaire50, "image binaire 0.5")
   #display (binaire55, "image binaire 0.55")
-  display (binaire60, "image binaire 0.60")
+  #display (binaire60, "image binaire 0.60")
 }
 
 # Chargement de l'image omega1
 
-nom <- "2classes_100_100_8bits_omega1_2016.png"
+#nom <- "2classes_100_100_8bits_omega1_2016.png"
+nom <- "rdf-chiffre-0-8bits_omega1.png"
 omega1 <- rdfReadGreyImage (nom)
 
 # Calcul et affichage de son histogramme
@@ -63,7 +65,8 @@ h1 <- hist (as.vector (omega1), freq=FALSE, breaks = seq (0, 1, 1 / nbins))
 
 p_omega1= sum(h1$counts[0:255])/ sum(h$counts[0:255])
 # a completer pour p_omega2
-nom <- "2classes_100_100_8bits_omega2_2016.png"
+#nom <- "2classes_100_100_8bits_omega2_2016.png"
+nom <- "rdf-chiffre-0-8bits_omega2.png"
 omega2 <- rdfReadGreyImage (nom)
 
 # Calcul et affichage de son histogramme
@@ -98,10 +101,9 @@ for (X in 1:255)
   {
 # (\sum_{\mathbf{X} \in \hat{\omega_2}} P(\mathbf{X} / \omega_1). P(\omega_1)  
   somme1[X+1]=sum(h1$density[(X+1):256])/sum(h1$density[1:256])
-    somme1[X+1]=somme1[X+1]*p_omega1
+  somme1[X+1]=somme1[X+1]*p_omega1
 #\sum_{\mathbf{X} \in \hat{\omega_1}} P(\mathbf{X}  / \omega_2). P(\omega_2)  
-# A complèter pour somme2  
-  somme2[X+1]=sum(h2$density[(X+1):256])/sum(h2$density[1:256])
+  somme2[X+1]=sum(h2$density[1:(X+1)])/sum(h2$density[1:256])
   somme2[X+1]=somme2[X+1]*p_omega2
 
   erreur[X+1] = somme1[X+1] + somme2[X+1]
@@ -109,7 +111,14 @@ for (X in 1:255)
   if (erreur[X+1] < minimum_erreur ) seuil_minimum_erreur = X
   if (erreur[X+1] < minimum_erreur ) minimum_erreur = erreur[X+1]
   }
-View(h$counts[142]/sum(h$counts[0:255]))
+
+pi141 = h$counts[142]/sum(h$counts[0:255])
+pomega1141 = h1$counts[142]/sum(h$counts[0:255])
+pomega2141 = h2$counts[142]/sum(h$counts[0:255])
+
+
 seuil = seuil_minimum_erreur/255 
+nom <- "rdf-chiffre-1-8bits.png"
+image <- rdfReadGreyImage (nom)
 binaire_Bayes <- (image - seuil) >= 0
 display (binaire_Bayes, "image binaire Bayes")
