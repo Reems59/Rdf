fc202c729f2f30f1a3002b4a9ec4b857d7068545
Dimitri Charneux <dimitri15.59@hotmail.fr>
2016-04-05 19:27:59 +0200

    trop long git add .

--- TP10/macro.R
+++ TP10/macro.R
@@ -61,24 +61,54 @@ entropie <- function(stackedFaces, etiquettes, booleans, branche){
 }
 
 createArbre <- function(stackedFaces, booleans, etiquettes){
+  nbTotal = length(stackedFaces[1,1,])
+  somme = sum(booleans)
+  stackedFaces1= array(0, dim=c(40,33,(nbTotal - somme))) 
+  stackedFaces2= array(0, dim=c(40,33,somme)) 
+  etiquettes1 = c()
+  etiquettes2 = c()
+  cpt1 = cpt2 =1
+  bool = 0
+  for(i in 1 : (nbTotal -1)){
+    if(etiquettes[i] != etiquettes[i+1]){
+      bool = 1
+    }
+  }
   
+  if(bool == 0){
+    cat("classe nÂ° ", etiquettes[1], "\n")
+    return()
+  } 
+  cat("on continue \n")
+  for(i in 1:nbTotal){
+    if(booleans[i] == 0){
+      stackedFaces1[,,cpt1] = stackedFaces[,,i]
+      etiquettes1[cpt1] = etiquettes[i]
+      cpt1 = cpt1 +1
+    }else{
+      stackedFaces2[,,cpt2] = stackedFaces[,,i]
+      etiquettes2[cpt2] = etiquettes[i]
+      cpt2 = cpt2 +1
+    }
+  }
+  
+  cat("un tour !! \n")
+  createSousArbre(stackedFaces1, etiquettes1)
+  createSousArbre(stackedFaces2, etiquettes2)
+}
+
+createSousArbre <- function(stackedFaces, etiquettes){
+  nbTotal = length(stackedFaces[1,1,])
+  booleans = array(0, dim = c(1,400))
+  cat("sous arbre \n")
   entropies1 = entropie(stackedFaces, etiquettes, booleans, 0)
-  entropies2 = entropie(stackedFaces, etiquettes, booleans, 1)
   
-  entropieMax1 = which(entropies1 == max(entropies1), arr.ind=TRUE)
-  View(which(entropies1 == max(entropies1), arr.ind=TRUE))
-  
-  entropieMax2 = which(entropies2 == max(entropies2), arr.ind=TRUE)
-  View(which(entropies2 == max(entropies2), arr.ind=TRUE))
-  
+  entropieMax = which(entropies1 == max(entropies1), arr.ind=TRUE)
   boolean <- c()
-  
   classCount = array(0, dim=c(2, 40))
   classRatio = array(0, dim = c(2,40))
- 
   
-  for(i in 1:400){
-    
+  for(i in 1:nbTotal){
     if(stackedFaces[entropieMax[1,1], entropieMax[1,2],i] < 0.5){
       boolean[i] = 0
       classCount[1,etiquettes[i]] = classCount[1,etiquettes[i]] + 1
@@ -87,12 +117,12 @@ createArbre <- function(stackedFaces, booleans, etiquettes){
       classCount[2,etiquettes[i]] = classCount[2,etiquettes[i]] + 1
     }
   }
-  
   for(i in 1:40){
     classRatio[1,i] = classCount[1, i]/10
     classRatio[2,i] = classCount[2, i]/10
-    
   }
+  
+  createArbre(stackedFaces, booleans, etiquettes)
 }
 
 
@@ -102,52 +132,47 @@ createArbre <- function(stackedFaces, booleans, etiquettes){
 nom <- "allFaces.png";
 image <- rdfReadGreyImage (nom)
 
-stackedFaces = initStackedFaces()
+stackedFacesT = initStackedFaces()
 
-etiquettes = c()
+etiquettesT = c()
 for(i in 1:400){
   if( i %% 10 == 0){
-    etiquettes[i] = as.integer(i/10)
+    etiquettesT[i] = as.integer(i/10)
   }else{
-    etiquettes[i] = as.integer(i / 10) +1
+    etiquettesT[i] = as.integer(i / 10) +1
   }
   
 }
 
-booleans = array(0, dim = c(1,400))
+booleansT = array(0, dim = c(1,400))
 
-entropies1 = entropie(stackedFaces, etiquettes, booleans, 0)
+entropies1T = entropie(stackedFacesT, etiquettesT, booleansT, 0)
 #entropies2 = entropie(stackedFaces, etiquettes, booleans, 1)
 
-entropieMax = which(entropies1 == max(entropies1), arr.ind=TRUE)
-View(which(entropies1 == max(entropies1), arr.ind=TRUE))
-boolean <- c()
-classCount = array(0, dim=c(2, 40))
-classRatio = array(0, dim = c(2,40))
-#on créée un tableau de boolean en fonction des probabilités du pixel de pivot dans chaque classe d'image
-# comme nous avons une probabilité pour chaque pixel, il faut definir un seuil pour mettre à 0 ou à 1 le pixel. Le seuil choisi est la moyenne des valeurs pour la classe ayant l'entropie la plus elevé.
+entropieMaxT = which(entropies1T == max(entropies1T), arr.ind=TRUE)
+View(which(entropies1T == max(entropies1T), arr.ind=TRUE))
+booleanT <- c()
+classCountT = array(0, dim=c(2, 40))
+classRatioT = array(0, dim = c(2,40))
+#on cr??e un tableau de boolean en fonction des probabilit?s du pixel de pivot dans chaque classe d'image
+# comme nous avons une probabilit? pour chaque pixel, il faut definir un seuil pour mettre ? 0 ou ? 1 le pixel. Le seuil choisi est la moyenne des valeurs pour la classe ayant l'entropie la plus elev?.
 
 for(i in 1:400){
-  if( i %% 10 == 0){
-    etiquettes[i] = as.integer(i/10)
-  }else{
-    etiquettes[i] = as.integer(i / 10) +1
-  }
-  
-  if(stackedFaces[entropieMax[1,1], entropieMax[1,2],i] < 0.5){
-    boolean[i] = 0
-    classCount[1,etiquettes[i]] = classCount[1,etiquettes[i]] + 1
+  if(stackedFacesT[entropieMaxT[1,1], entropieMaxT[1,2],i] < 0.5){
+    booleanT[i] = 0
+    classCountT[1,etiquettesT[i]] = classCountT[1,etiquettesT[i]] + 1
   } else {
-    boolean[i] = 1
-    classCount[2,etiquettes[i]] = classCount[2,etiquettes[i]] + 1
+    booleanT[i] = 1
+    classCountT[2,etiquettesT[i]] = classCountT[2,etiquettesT[i]] + 1
   }
 }
 for(i in 1:40){
-  classRatio[1,i] = classCount[1, i]/10
-  classRatio[2,i] = classCount[2, i]/10
+  classRatioT[1,i] = classCountT[1, i]/10
+  classRatioT[2,i] = classCountT[2, i]/10
   
 }
 
+createArbre(stackedFacesT, booleansT, etiquettesT)
 
 
 
